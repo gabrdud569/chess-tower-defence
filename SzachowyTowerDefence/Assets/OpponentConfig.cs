038c1d9d3d6a2f7ba402dc4b5793b69d7405b34b
domifig613 <32535456+domifig613@users.noreply.github.com>
2020-11-13 17:49:20 +0100

    Add some background to scene

---
+++ SzachowyTowerDefence/Assets/OpponentConfig.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "OpponentConfig", menuName = "OpponentConfig")]
+public class OpponentConfig : ScriptableObject
+{
+    [SerializeField] public int maxLife;
+    [SerializeField] public int speed;
+    [SerializeField] public int damage;
+}
