e7d1c7c91c8319125d31701f84279b0c749d1652
Dominik Figlak <dominikfiglak@interia.pl>
2020-11-13 18:35:02 +0100

    Add path config file

---
+++ SzachowyTowerDefence/Assets/PathConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PathConfig", menuName = "PathConfig")]
+public class PathConfig : ScriptableObject
+{
+    [SerializeField] public List<string> path;
+}
