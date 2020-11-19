b1a664e857167ddb95cf901623fc59c079e8853e
sondisonda <kubasadel97@gmail.com>
2020-11-19 14:05:19 +0100

    Set of figures

---
+++ SzachowyTowerDefence/Assets/Scripts/FiguresManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FiguresManager : MonoBehaviour
+{
+    public static FiguresManager instance;
+
+    void Awake ()
+    {
+        if (instance !=null)
+        { 
+            Debug.LogError("More than one FiguresManager!")
+        }
+        instance = this;
+    }
+
+    private GameObject figureToPlace;
+
+    public GameObject GetFigureToPlace()
+    {
+        return figureToPlace;
+    }
+
+
+}
