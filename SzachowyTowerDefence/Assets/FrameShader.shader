4b8f25a77ffebbb5f74303847c5e208886bead91
Dominik Figlak <domifig613@student.polsl.pl>
2020-11-25 19:21:53 +0100

    Add detect face

---
+++ SzachowyTowerDefence/Assets/FrameShader.shader
@@ -0,0 +1,69 @@
+﻿Shader "Custom/FrameShader"
+{
+    Properties
+    {
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+    }
+        SubShader
+        {
+            Tags { "RenderType" = "Opaque" }
+            LOD 100
+
+            Pass
+            {
+                CGPROGRAM
+                #pragma vertex vert
+                #pragma fragment frag
+                // make fog work
+                #pragma multi_compile_fog
+
+                #include "UnityCG.cginc"
+
+
+                struct appdata
+                {
+                float4 vertex : POSITION;
+                float2 uv : TEXCOORD0;
+                };
+
+                struct v2f
+                {
+                    float2 uv : TEXCOORD0;
+                    float4 vertex : SV_POSITION;
+                };
+
+                sampler2D _MainTex;
+                float tlx;
+                float tly;
+                float brx;
+                float bry;
+
+
+                v2f vert(appdata v)
+                {
+                    v2f o;
+                    o.vertex = UnityObjectToClipPos(v.vertex);
+                    o.uv = v.uv;
+                    return o;
+                }
+
+                fixed4 frag(v2f i) : SV_Target
+                {
+                    fixed4 col = tex2D(_MainTex, i.uv);
+                    float x = 1 - i.uv.x;
+                    float y = 1 - i.uv.y;
+
+                   if((((x < tlx + brx && x > tlx + brx - 0.01) || (x > tlx && x < tlx + 0.01)) 
+                       || ((y > tly && y < tly + 0.01) || (y < tly + bry && y > tly + bry - 0.01))) 
+                       && (x > tlx && x < tlx + brx) && (y > tly && y < tly + bry))
+                   {    
+                      col = fixed4(1, 0, 0, 1);
+                   }
+
+                   return col;
+                }
+            ENDCG
+                }
+        }
+                Fallback "Diffuse"
+}
--- SzachowyTowerDefence/Assets/testOpenCV.cs
+++ SzachowyTowerDefence/Assets/testOpenCV.cs
@@ -17,6 +17,7 @@ public class testOpenCV : MonoBehaviour
     MeshRenderer renderer;
     WebCamTexture webcamTexture;
     Texture defalultTexture;
+    bool run = false;
 
     void Start()
     {
@@ -27,25 +28,37 @@ public class testOpenCV : MonoBehaviour
 
     public void Enable()
     {
-        Texture2D t = new Texture2D(renderer.material.mainTexture.width, renderer.material.
-        mainTexture.height, TextureFormat.RGB24, false);
         webcamTexture = new WebCamTexture();
         renderer.sharedMaterial.mainTexture = webcamTexture;
         webcamTexture.Play();
-        //t.SetPixels(webcamTexture.GetPixels());
-        //t.Apply();
-        //byte[] bytes = t.GetRawTextureData();
-        //float tlx = 0, tly = 0, brx = 0, bry = 0;
-        //loadClassifier();
-        //detectChessPiece(bytes.Length, t.width, t.height, bytes, out tlx, out tly, out brx, out bry);
-        //Debug.Log(" FaceCoords :(" + tlx + ", " + tly + ", " + brx + ", " + bry + " ,) ");
-        //renderer.sharedMaterial.SetFloat("CX", 1 - (tlx + brx / 2) / t.width);
-        //renderer.sharedMaterial.SetFloat("CY", (tly + bry / 2) / t.height);
-        //renderer.sharedMaterial.SetFloat("R", bry / (2 * t.height));
+        renderer.sharedMaterial.SetTexture("MainTex", webcamTexture);
+        run = true;
     }
+
+    private void Update()
+    {
+        if (run)
+        {
+            Texture2D t = new Texture2D(renderer.material.mainTexture.width, renderer.material.mainTexture.height, TextureFormat.RGB24, false);
+            t.SetPixels(webcamTexture.GetPixels());
+            t.Apply();
+            byte[] bytes = t.GetRawTextureData();
+            float tlx = 0, tly = 0, brx = 0, bry = 0;
+            loadClassifier();
+            detectChessPiece(bytes.Length, t.width, t.height, bytes, out tlx, out tly, out brx, out bry);
+            Debug.Log(" FaceCoords :(" + tlx + ", " + tly + ", " + brx + ", " + bry + " ,) ");
+            renderer.sharedMaterial.SetFloat("tlx", tlx/webcamTexture.width);
+            renderer.sharedMaterial.SetFloat("tly", tly / webcamTexture.height);
+            renderer.sharedMaterial.SetFloat("brx",  brx/ webcamTexture.width);
+            renderer.sharedMaterial.SetFloat("bry", bry/ webcamTexture.height);
+            Debug.Log(webcamTexture.width + " " + webcamTexture.height);
+        }
+    }
+
 
     public void Disable()
     {
+        run = false;
         renderer.material.mainTexture = defalultTexture;
         webcamTexture.Stop();
     }
